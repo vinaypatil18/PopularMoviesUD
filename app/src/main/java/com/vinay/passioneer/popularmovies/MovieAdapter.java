291ac2c526b3edb21b4acbf2da1476a09d378d0c
vinaypatil18 <vinay18.patil@gmail.com>
2016-01-03 23:44:23 +0530

    Showed the heart icon, popularity percent and votes on Grid.

--- app/src/main/java/com/vinay/passioneer/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/vinay/passioneer/popularmovies/MovieAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.vinay.passioneer.popularmovies.model.MovieModel;
@@ -25,22 +26,29 @@ public class MovieAdapter extends ArrayAdapter<MovieModel> {
         this.posterWidth = actualPosterViewWidth;
         //keeping height greater than the width
         // so dividing by 0.66
-        this.posterHeight = (int)(posterWidth/0.66);
+        this.posterHeight = (int) (posterWidth / 0.66);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         MovieModel movieModel = getItem(position);
 
-        if(convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_grid_item,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_grid_item, parent, false);
         }
 
-        ImageView imageView = (ImageView)convertView.findViewById(R.id.movie_poster_image);
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.movie_poster_image);
         Picasso.with(getContext())
                 .load(movieModel.getPosterPath())
-                .resize(posterWidth,posterHeight)
+                .resize(posterWidth, posterHeight)
                 .into(imageView);
+
+        TextView popularity_percent = (TextView) convertView.findViewById(R.id.popularity_percent);
+        long percent = Math.round(movieModel.getVoteAverage() * 10);
+        popularity_percent.setText(String.valueOf(percent) + "%");
+
+        TextView vote_count = (TextView) convertView.findViewById(R.id.vote_count);
+        vote_count.setText(String.valueOf(movieModel.getVoteCount())+" votes");
         return convertView;
     }
 }
